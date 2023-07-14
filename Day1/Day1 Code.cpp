6f66438e37366877ab8724c77efc55d0e09c6ade
Deva <devakumartadicherla2001@gmail.com>
2023-07-14 20:37:01 +0530

    update

---
+++ Day1/Day1 Code.cpp
@@ -0,0 +1,24 @@
+//POTD Day July 14
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
+        int n = arr.size();
+        unordered_map<int, int> dp;
+        int ans = 1;
+        for (int i = 0; i < n; i++) 
+        {
+            int num = arr[i];
+            if (dp.find(num - difference) != dp.end()) // checking for the difference value
+            {
+                dp[num] = dp[num - difference] + 1;
+            } 
+            else 
+            {
+                dp[num] = 1;
+            }
+            ans = max(ans, dp[num]); // return upto maximum value
+        }
+        return ans;
+    }
+};
